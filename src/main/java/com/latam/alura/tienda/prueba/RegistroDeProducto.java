2d6ea2e39fc328d9997dcbb614f31fd79262405a
d4viss <davisonju26@gmail.com>
2023-08-23 09:13:40 -0500

    prueba de creación de la entidad en ela base de datos

---
+++ src/main/java/com/latam/alura/tienda/prueba/RegistroDeProducto.java
@@ -0,0 +1,25 @@
+package com.latam.alura.tienda.prueba;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.latam.alura.tienda.modelo.Producto;
+
+public class RegistroDeProducto {
+
+	public static void main(String[] args) {
+		Producto celular = new Producto();
+		celular.setNombre("Samsung");
+		celular.setDescripcion("Telefono usado");
+		celular.setPrecio(new BigDecimal("1000"));
+		
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tienda");
+		EntityManager manager = factory.createEntityManager();
+		
+		manager.persist(celular);
+	}
+
+}
